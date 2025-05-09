30d2725faa5103d0e2cbce7c6bca8a6d15b5dd12
novak-stevanovic <novakst24@gmail.com>
2025-05-09 20:08:17 +0200

    .

--- include/nt_terms/_nt_terms.h
+++ include/nt_terms/_nt_terms.h
@@ -11,41 +11,39 @@ struct nt_esc_input_code
     uint8_t seq[20];
 };
 
-#define NT_ESC_KEY_COUNT 25
-
 struct nt_term
 {
-    struct nt_esc_input_code keys[NT_ESC_KEY_COUNT];
+    struct nt_esc_input_code* keys;
 
-    void (*show_cursor_func)();
-    void (*hide_cursor_func)();
-    void (*move_cursor_func)(size_t x, size_t y);
+    char* show_cursor_code;
+    char* hide_cursor_code;
+    char* move_cursor_code;
 
-    void (*set_fg_c8_func)(uint8_t code);
-    void (*set_fg_c256_func)(uint8_t code);
-    void (*set_fg_rgb_func)(uint8_t r, uint8_t g, uint8_t b);
+    char* set_fg_c8_code;
+    char* set_fg_c256_code;
+    char* set_fg_rgb_code;
 
-    void (*set_bg_c8_func)(uint8_t code);
-    void (*set_bg_c256_func)(uint8_t code);
-    void (*set_bg_rgb_func)(uint8_t r, uint8_t g, uint8_t b);
+    char* set_bg_c8_code;
+    char* set_bg_c256_code;
+    char* set_bg_rgb_code;
 
-    void (*set_style_bold_func)();
-    void (*set_style_faint_func)();
-    void (*set_style_italic_func)();
-    void (*set_style_underline_func)();
-    void (*set_style_blink_func)();
-    void (*set_style_reverse_func)();
-    void (*set_style_hidden_func)();
-    void (*set_style_strikethrough_func)();
+    char* set_style_bold_code;
+    char* set_style_faint_code;
+    char* set_style_italic_code;
+    char* set_style_underline_code;
+    char* set_style_blink_code;
+    char* set_style_reverse_code;
+    char* set_style_hidden_code;
+    char* set_style_strikethrough_code;
 
-    void (*reset_gfx_func)();
+    char* reset_gfx_code;
 
-    void (*erase_screen_func)();
-    void (*erase_scrollback_func)();
-    void (*erase_line_func)();
+    char* erase_screen_code;
+    char* erase_scrollback_code;
+    char* erase_line_code;
 
-    void (*enter_alt_buff_func)();
-    void (*exit_alt_buff_func)();
+    char* enter_alt_buff_code;
+    char* exit_alt_buff_code;
 };
 
 void nt_terms_init();
